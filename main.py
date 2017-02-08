9c3365aba77617c15dc41c1ca59c7c6fb8729e03
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-02-09 00:12:46 +0900

    Add: add a color scale

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ import math
 from mpl_toolkits.mplot3d import Axes3D
 mpl.rcParams['legend.fontsize'] = 10
 
+import matplotlib.cm as cm
+
+def colorscale(v):
+    t = math.cos(4 * math.pi * v)
+    c = int(((-t / 2) + 0.5) * 255)
+    if v >= 1.0:
+        return '#%02X%02X%02X' % (255, 0, 0)
+    elif v >= 0.75:
+        return '#%02X%02X%02X' % (255, c, 0.0)
+    elif v >= 0.5:
+        return '#%02X%02X%02X' % (c, 255, 0)
+    elif v >= 0.25:
+        return '#%02X%02X%02X' % (0, 255, c)
+    elif v >= 0:
+        return '#%02X%02X%02X' % (0, c, 255)
+    else:
+        return '#%02X%02X%02X' % (0, 0, 255)
+
 f = open('./data.csv', 'r')
 reader = csv.reader(f)
 header = next(reader)
@@ -14,18 +32,22 @@ dimension = []
 C = []
 gamma = []
 accuracy = []
+colorlist = []
 
 for row in reader:
     dimension.append(int(row[0]))
     C.append(float(row[1]))
     gamma.append(float(row[2]))
     accuracy.append(float(row[3]))
+    colorlist.append(colorscale(float(row[3])))
 
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.set_xlabel("dimension")
 ax.set_ylabel("C")
 ax.set_zlabel("gamma")
-ax.plot(dimension, C, gamma, "o", color="#cccccc", ms=4, mew=0.5)
+
+for (i, x) in enumerate(dimension):
+    ax.plot([dimension[i]], [C[i]], [gamma[i]], "o", color=colorlist[i], ms=4, mew=0.5)
 ax.legend()
 plt.show()
