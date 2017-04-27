c296893028a83964a8dd94f6d151a1781c3b03f3
KozenUmezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-04-27 15:05:09 +0900

    Add:

--- word2vec_2d.py
+++ word2vec_2d.py
@@ -13,33 +13,35 @@ f_status = open('./word2vec_full_vectors/word2vec_100.csv', 'r')
 reader_status = csv.reader(f_status)
 header_status = next(reader_status)
 color_list = []
+id_list =[]
 for row in reader_status:
+    id_list.append(row[0])
     if row[1] == 'Accept':
         color_list.append('blue')
     else:
         color_list.append('red')
 
 
-f = open('./converted_to_3d/converted_3d_80.csv', 'r')
+f = open('./converted_to_2d/converted_2d_80.csv', 'r')
 reader = csv.reader(f)
 header = next(reader)
 X = []
 Y = []
-Z = []
-
-for row in reader:
-    X.append(float(row[0]))
-    Y.append(float(row[1]))
-    Z.append(float(row[2]))
+for (i, row) in enumerate(reader):
+    x = float(row[0])
+    y = float(row[1])
+    if x > 26.5 and y > -10 and y < 0:
+        print i + 1, id_list[i], x, y
+        X.append(x)
+        Y.append(y)
 
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
-ax.set_zlabel("Z")
 
 for (i, x) in enumerate(X):
-    ax.plot([X[i]], [Y[i]], [Z[i]], "o", color=color_list[i], ms=4, mew=0.5)
+    ax.plot([X[i]], [Y[i]], "o", color=color_list[i], ms=4, mew=0.5)
 ax.legend()
 
 plt.show()
